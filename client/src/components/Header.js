b04c22159215f26620593171a3e81ae50ea2bb6a
Aman Mehrat <aman.mehrat956@gmail.com>
2020-11-18 23:51:32 +0530

    Implement Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Search, Language, ExpandMore } from '@material-ui/icons'
+import { Avatar } from '@material-ui/core'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header">
+            <img
+                className="header__icon"
+                src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                alt="airbnb logo"
+            />
+            <div className="header__center">
+                <input type="text" />
+                <Search />
+            </div>
+            <div className="header__right">
+                <p>Beacome a host</p>
+                <Language />
+                <ExpandMore />
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+export default Header;
